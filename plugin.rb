3e6ed784f130176b35b18d6d7b07d3131eb7ffeb
Robert Guthrie <rguthrie@gmail.com>
2018-07-17 15:03:49 +0200

    sane default rather than blow up

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,6 @@ module Plugins
   module LoomioLegal
     class Plugin < Plugins::Base
       setup! :loomio_legal_plugin do |plugin|
-        raise "LEGAL_DIR ENV is required" unless ENV['LEGAL_DIR']
         plugin.enabled = true
         plugin.use_view_path :"app/views"
         plugin.use_class_directory 'app/controllers'
