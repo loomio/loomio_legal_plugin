e447b42c6f6ca8f896bfcf1e146bfe88060862c5
Robert Guthrie <rob@guthrie.nz>
2018-10-09 15:12:34 +1300

    test for crazy

--- app/controllers/legal_controller.rb
+++ app/controllers/legal_controller.rb
@@ -1,10 +1,16 @@
 class LegalController < ApplicationController
   layout 'basic'
 
-  %w[privacy terms_of_service third_parties].each do |name|
-    define_method name do
-      render "legal/#{ENV.fetch('LEGAL_DIR', 'www.loomio.org')}/#{name}.html.md"
-    end
+  def privacy
+    render "legal/#{ENV.fetch('LEGAL_DIR', 'www.loomio.org')}/privacy.html.md"
+  end
+
+  def terms_of_service
+    render "#{ENV.fetch('LEGAL_DIR', 'www.loomio.org')}/terms_of_service.html.md"
+  end
+
+  def third_parties
+    render "/legal/#{ENV.fetch('LEGAL_DIR', 'www.loomio.org')}/third_parties.html.md"
   end
 
   def gdpr
